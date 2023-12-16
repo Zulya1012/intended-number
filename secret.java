12d69fb18c7d54ede840248524fd4eac3f3c297f
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-16 16:25:52 +0400

    Update secret.java

--- secret.java
+++ secret.java
@@ -1,25 +1,20 @@
 import java.util.Scanner;
- public class secret{
- public static void main(String[] arg){
-Scanner s=new Scanner(System.in);
-int b=10;
-System.out.println("Vvedite chislo:");
-int a=s.nextInt();
+public class secret {
+  public static void main(String[] arg) {
+    Scanner s = new Scanner(System.in);
+    int b = 10;
+    System.out.println("Enter the number:");
+    int a = s.nextInt();
 
-int x=a-b;
+    int x = a - b;
 
-if (x>30){
-  System.out.println("Xolodno");
-}
-else if (x==0){
-System.out.println("Uqadano");
-}
-else if (x<30){
-  System.out.println("Teplee");
-}
-}
+    if (x > 30) {
+      System.out.println("Cold");
+    } else if (x == 0) {
+      System.out.println("Guessed");
+    } else if (x < 30) {
+      System.out.println("Warmer");
+    }
+  }
 
 }
-
-
-
